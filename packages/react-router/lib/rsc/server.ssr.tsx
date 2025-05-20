8c5440234bad15acb64c50b44b2f456bff768f35
Master-Hash <hash@hash.memorial>
2025-05-20 23:14:48 +0200

    feat: support nonce

--- packages/react-router/lib/rsc/server.ssr.tsx
+++ packages/react-router/lib/rsc/server.ssr.tsx
@@ -12,6 +12,7 @@ export async function routeRSCServerRequest({
   decode,
   renderHTML,
   hydrate = true,
+  nonce,
 }: {
   request: Request;
   callServer: (request: Request) => Promise<Response>;
@@ -20,6 +21,7 @@ export async function routeRSCServerRequest({
     getPayload: () => Promise<ServerPayload>
   ) => ReadableStream<Uint8Array> | Promise<ReadableStream<Uint8Array>>;
   hydrate?: boolean;
+  nonce?: string;
 }) {
   const url = new URL(request.url);
   const isDataRequest = isReactServerRequest(url);
@@ -71,7 +73,9 @@ export async function routeRSCServerRequest({
       throw new Error("Failed to clone server response");
     }
 
-    const body = html.pipeThrough(injectRSCPayload(serverResponseB.body));
+    const body = html.pipeThrough(injectRSCPayload(serverResponseB.body, {
+      nonce,
+    }));
     return new Response(body, {
       status: serverResponse.status,
       headers,
@@ -90,8 +94,10 @@ export async function routeRSCServerRequest({
 
 export function RSCStaticRouter({
   getPayload,
+  nonce,
 }: {
   getPayload: () => Promise<ServerPayload>;
+  nonce?: string;
 }) {
   // @ts-expect-error - need to update the React types
   const payload = React.use(getPayload()) as ServerPayload;
@@ -186,7 +192,7 @@ export function RSCStaticRouter({
           context={context}
           router={router}
           hydrate={false}
-          nonce={payload.nonce}
+          nonce={nonce}
         />
       </FrameworkContext.Provider>
     </RSCRouterContext.Provider>
--- playground/rsc-vite/src/ssr/entry.ssr.tsx
+++ playground/rsc-vite/src/ssr/entry.ssr.tsx
@@ -12,6 +12,8 @@ import {
   unstable_RSCStaticRouter as RSCStaticRouter,
 } from "react-router";
 
+import { NonceContext } from "./nonce.client.tsx";
+
 type CloudflareEnv = {
   ASSETS: Fetcher;
   SERVER: Fetcher;
@@ -20,21 +22,38 @@ type CloudflareEnv = {
 export default {
   async fetch(request, { SERVER }) {
     const callServer = async (request: Request) => await SERVER.fetch(request);
+    const nonce = btoa(
+      String.fromCharCode(...crypto.getRandomValues(new Uint8Array(32))),
+    );
     try {
-      return await routeRSCServerRequest({
+      const z = await routeRSCServerRequest({
         request,
         callServer,
         decode: (body) => RSD.createFromReadableStream(body, manifest),
         async renderHTML(getPayload) {
           return await RDS.renderToReadableStream(
-            <RSCStaticRouter getPayload={getPayload} />,
+
+            <NonceContext value={nonce}>
+              <RSCStaticRouter getPayload={getPayload} nonce={nonce} />
+            </NonceContext>,
             {
               bootstrapModules,
               signal: request.signal,
+              nonce,
             }
           );
         },
+        hydrate: true,
+        nonce,
       });
+      z.headers.set(
+        "content-security-policy",
+        `default-src 'self'; style-src-attr 'self' 'unsafe-inline'; script-src 'self'; script-src-elem 'self' 'nonce-${nonce}'; img-src 'self' data:` +
+        (import.meta.env.DEV
+          ? "; style-src-elem 'unsafe-inline'"
+          : "; connect-src 'self' https://cloudflareinsights.com/"),
+      );
+      return z;
     } catch (reason) {
       console.error(reason);
       return new Response("Internal Server Error", { status: 500 });
